fec07e7986bdee06b1e4c5f75ab8fd77e5f93a94
alswns <49472611+alswns@users.noreply.github.com>
2020-04-29 02:12:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.alert import Alert
 import time
 driver = webdriver.Chrome('./chromedriver')
 driver.get('https://hoc22.ebssw.kr/sso/loginView.do?loginType=onlineClass')
-driver.find_element_by_name('j_username').send_keys('alswns030221')
-driver.find_element_by_name('j_password').send_keys('alswns0221')
+driver.find_element_by_name('j_username').send_keys('user_id')
+driver.find_element_by_name('j_password').send_keys('user_pw')
 driver.find_element_by_class_name('img_type').click()
 time.sleep(0.5)
 driver.get('https://hoc22.ebssw.kr/20sr205/hmpg/hmpgBbsListView.do?menuSn=411966&bbsId=BBSID_000399096')
